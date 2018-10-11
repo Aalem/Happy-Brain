a9b3c51949cbe8ce8ed69800e1099aa0a7f8d4bf
Aalem Daneshyar <aalem.daneshyar87@gmail.com>
2018-10-11 08:26:31 +0430

    third commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express=require('express');
+const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -9,12 +9,12 @@ const config = require('./config/database');
 //Connect to DB
 mongoose.connect(config.database);
 //On Connection
-mongoose.connection.on('connected',function () {
-    console.log("Connected to DB " +config.database);
+mongoose.connection.on('connected', function () {
+    console.log("Connected to DB " + config.database);
 });
 //On Error
-mongoose.connection.on('error',function (err) {
-    console.log("Database error " +err);
+mongoose.connection.on('error', function (err) {
+    console.log("Database error " + err);
 });
 
 const app = express();
@@ -35,7 +35,16 @@ const port = 3000;
 app.use(cors());
 
 //Set static folders and files
-app.use(express.static(path.join(__dirname,'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'public')));
+}
+
+app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'public'));
+});
+
 
 //Body parser middleware
 app.use(bodyParser.json());
@@ -46,9 +55,9 @@ app.use(passport.session());
 
 require('./config/passport')(passport);
 
-app.use('/admins',admins);
-app.use('/students',students);
-app.use('/mentor_subjects',mentors);
+app.use('/admins', admins);
+app.use('/students', students);
+app.use('/mentor_subjects', mentors);
 
 //Subjects routes
 app.use('/subjects', subjects);
@@ -62,11 +71,13 @@ app.use('/mentoring-meeting', mentoring_meeting);
 app.use('/survey', survey);
 
 //index routes
-app.get('/',function (req,res) {
+app.get('/login', function (req, res) {
     res.send('Invalid ');
 });
 
+app.set('port', process.env.PORT || 3000);
+
 //Start server
-app.listen(process.env.PORT || 3000 ,function(){
-    console.log("Server started on port"+process.env.PORT);
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server started on port" + process.env.PORT);
 });
