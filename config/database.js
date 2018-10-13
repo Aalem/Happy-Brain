1031485753680b4274ceb445c91c4d55baefc20f
Aalem Daneshyar <aalem.daneshyar87@gmail.com>
2018-10-13 11:00:59 +0430

    4th Comment

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
-module.exports={
-    database:'mongodb://localhost:27017/happy-brain',
-    secret:'yoursecret'
-}
-
 // module.exports={
-//     database:'mongodb://admin:admin1@ds125713.mlab.com:25713/happy-brain',
+//     database:'mongodb://localhost:27017/happy-brain',
 //     secret:'yoursecret'
 // }
+
+module.exports={
+    database:'mongodb://admin:admin1@ds125713.mlab.com:25713/happy-brain',
+    secret:'yoursecret'
+}
